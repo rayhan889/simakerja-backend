9de3b68c058860c1fe4d937e1c2998f5b1a77a3f
rayhan889 <raydev1604@gmail.com>
2026-02-02 14:10:31 +0700

    init

---
+++ src/main/java/com/rynrama/simakerjabackend/SimakerjaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rynrama.simakerjabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimakerjaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimakerjaBackendApplication.class, args);
+    }
+
+}
